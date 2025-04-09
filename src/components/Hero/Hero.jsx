b28f57365be5fa5abdfa612b8332b76742916fcf
youssif-shehata <youssif.shehata66@gmail.com>
2025-04-10 00:14:36 +0400

    My portfolio

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -14,7 +14,6 @@ function Hero() {
             I’m a front-end developer with 1 year of experience using React.
             Reach out if you’d like to learn more!
           </p>
-          <p>I’m a Backend Developer</p>
           <a
             href="mailto:youssif.shehata66@gmail.com"
             className={styles.contactBtn}
