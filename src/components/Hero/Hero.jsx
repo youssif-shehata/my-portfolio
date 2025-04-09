4553c5f28f14e4979564762364b8de7e1d54b675
youssif-shehata <youssif.shehata66@gmail.com>
2025-04-09 23:26:49 +0400

    Added new welcome feature

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,30 +1,36 @@
 import styles from "./Hero.module.css";
 
 function Hero() {
+  const welcome = function () {
+    "Welcome to the application";
+  };
   return (
-    <section className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={styles.title}>Hi, I’m Youssef</h1>
-        <p className={styles.description}>
-          I’m a front-end developer with 1 year of experience using React. Reach
-          out if you’d like to learn more!
-        </p>
-        <p>I’m a Backend Developer</p>
-        <a
-          href="mailto:youssif.shehata66@gmail.com"
-          className={styles.contactBtn}
-        >
-          Contact Me
-        </a>
-      </div>
-      <img
-        src={"/assets/hero/heroImage.png"}
-        alt="Hero image of me"
-        className={styles.heroImg}
-      />
-      <div className={styles.topBlur} />
-      <div className={styles.bottomBlur} />
-    </section>
+    <>
+      welcome()
+      <section className={styles.container}>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Hi, I’m Youssef</h1>
+          <p className={styles.description}>
+            I’m a front-end developer with 1 year of experience using React.
+            Reach out if you’d like to learn more!
+          </p>
+          <p>I’m a Backend Developer</p>
+          <a
+            href="mailto:youssif.shehata66@gmail.com"
+            className={styles.contactBtn}
+          >
+            Contact Me
+          </a>
+        </div>
+        <img
+          src={"/assets/hero/heroImage.png"}
+          alt="Hero image of me"
+          className={styles.heroImg}
+        />
+        <div className={styles.topBlur} />
+        <div className={styles.bottomBlur} />
+      </section>
+    </>
   );
 }
 
